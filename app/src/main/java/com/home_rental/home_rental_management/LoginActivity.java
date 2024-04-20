a5f829483355661e5d6ffd6eeec1ed2780b61a46
Masud2017 <msmasud578@gmail.com>
2024-04-20 15:42:29 +0600

    Added LoginActivity, button style,home_card_style, activity_home and also resolved issues related to scrolling grid view data also back button issue

--- app/src/main/java/com/home_rental/home_rental_management/LoginActivity.java
+++ app/src/main/java/com/home_rental/home_rental_management/LoginActivity.java
@@ -2,6 +2,7 @@ package com.home_rental.home_rental_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        // code here to show dialog
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+        finish();
+        super.onBackPressed();  // optional depending on your needs
     }
 }
