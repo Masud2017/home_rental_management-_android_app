3c7db5a0099cea283291d59a6f188556f023ab05
Masud2017 <msmasud578@gmail.com>
2024-04-20 16:01:26 +0600

    Added signup activity

--- app/src/main/java/com/home_rental/home_rental_management/HomeActivity.java
+++ app/src/main/java/com/home_rental/home_rental_management/HomeActivity.java
@@ -7,23 +7,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.home_rental.home_rental_management.Models.Home;
+
 public class HomeActivity extends AppCompatActivity {
     private AppCompatButton loginBtn = null;
-    private AppCompatButton getLoginBtn = null;
+    private AppCompatButton signupButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         this.loginBtn = findViewById(R.id.login);
+        this.signupButton = findViewById(R.id.signup);
 
-        this.loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loginIntent = new Intent(HomeActivity.this,LoginActivity.class);
-                startActivity(loginIntent);
-                finish();
-            }
+        this.loginBtn.setOnClickListener(view -> {
+            Intent loginIntent = new Intent(HomeActivity.this,LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        });
+
+        this.signupButton.setOnClickListener((View view)-> {
+            Intent intent = new Intent(HomeActivity.this, SignupActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
