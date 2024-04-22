fccb5a722240e92ed88f1b65f4f13da484d28113
Masud2017 <msmasud578@gmail.com>
2024-04-22 14:33:24 +0600

    Changed the android default logo to user defined one and also added functionalities to the AdminDashBoardActivity

--- app/src/main/java/com/home_rental/home_rental_management/AdminDashBoardActivity.java
+++ app/src/main/java/com/home_rental/home_rental_management/AdminDashBoardActivity.java
@@ -1,14 +1,24 @@
 package com.home_rental.home_rental_management;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AdminDashBoardActivity extends AppCompatActivity {
+    private AppCompatImageView addHomeButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dash_board);
+
+        this.addHomeButton = findViewById(R.id.add_home_btn);
+
+        this.addHomeButton.setOnClickListener(view -> {
+            Intent openAddHomeActivity = new Intent(AdminDashBoardActivity.this, AddHomeActivity.class);
+            startActivity(openAddHomeActivity);
+        });
     }
 }
